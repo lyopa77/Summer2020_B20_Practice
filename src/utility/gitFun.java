eb90eae7b7887475dfc6014a38bb03c13546389f
lyopa77 <68960927+lyopa77@users.noreply.github.com>
2020-08-05 20:39:27 -0500

    Update gitFun.java

--- src/utility/gitFun.java
+++ src/utility/gitFun.java
@@ -3,7 +3,7 @@ package utility;
 public class gitFun {
     public static void main(String[] args) {
         System.out.println("Fun");
-        System.out.println("java is fun");
+        System.out.println("java is fun for all");
         
     }
 }
