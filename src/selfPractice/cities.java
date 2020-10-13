86a37bdecb5e53daf56014856078aef5716b1648
lyopa77 <68960927+lyopa77@users.noreply.github.com>
2020-10-12 23:22:33 -0400

    Update cities.java

--- src/selfPractice/cities.java
+++ src/selfPractice/cities.java
@@ -21,7 +21,7 @@ public class cities {
         System.out.println("From " + city1+ " to "+ city2 + " is $500");
         System.out.println("From " + city3+ " to "+ city1 + " is $550");
         System.out.println("I have been to " + city1 +", "+ city2 +", " + city3+ ".");
-        System.out.println("bye");
+        System.out.println("bye, bye");
 
 
 
